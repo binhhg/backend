c38a8d4e87b861eaad83ca60ad53c6ad18864c24
binhpt <binhpt@carpla.vn>
2025-04-26 15:52:13 +0700

    wa

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,7 @@
 module.exports = (container) => {
   const userController = require('./userController')(container)
   const categoryController = require('./categoryController')(container)
+
   const articleController = require('./articleController')(container)
   const commentController = require('./commentController')(container)
   const authorizationController = require('./authorizationController')(container)
